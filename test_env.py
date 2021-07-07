bbbbdaa8b1e2af0ff68158c9a03e0cdbd5df0ff5
zebexx <andyfab89@gmail.com>
2021-07-07 12:50:37 +0100

    First working dqn

--- test_env.py
+++ test_env.py
@@ -1,8 +1,52 @@
 import gym
+import random
+import numpy as np
+from IPython.display import clear_output
+import time
 
-env = gym.make('CarRacing-v0')
-env.reset()
-for _ in range(1000):
-    env.render()
-    env.step(env.action_space.sample())
-env.close()
+
+
+env = gym.make("Taxi-v3").env
+q_table = np.zeros([env.observation_space.n, env.action_space.n])
+
+# hyperparameters
+learning_rate = 0.1
+discount_rate = 0.6
+exploration_rate = 0.1
+
+# plotting metrics
+all_epochs = []
+all_penalties = []
+
+for i in range(1, 100001):
+    state = env.reset()
+
+    epochs, penalties, reward, = 0, 0, 0
+    done = False
+
+    while not done:
+        if random.uniform(0, 1) < exploration_rate:
+            action = env.action_space.sample() # Explore action space
+        else:
+            action = np.argmax(q_table[state]) # Exploit learned values
+        
+        next_state, reward, done, info = env.step(action)
+
+        old_value = q_table[state, action]
+        next_max = np.max(q_table[next_state])
+
+        new_value = (1 - learning_rate) * old_value + learning_rate * (reward + discount_rate * next_max)
+        q_table[state, action] = new_value
+
+        if reward == -10:
+            penalties += 1
+
+        state = next_state
+        epochs += 1
+
+    if i % 100 == 0:
+        clear_output(wait=True)
+        print("Episode: {}".format(i))
+
+print("Training finished.\n")
+print(q_table[328])
