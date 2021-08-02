7586ac6ac1889bffde9fd478983c16ab1da89b21
zebexx <andyfab89@gmail.com>
2021-08-02 22:32:41 +0100

    plot.py can generate graphs from graphData saves

--- ddqn/plot.py
+++ ddqn/plot.py
@@ -1,4 +1,23 @@
-import numpy as np
 import matplotlib.pyplot as plt
+import numpy as np
+import os
+from utils import plot
 
-folder = "graphData/0"
+def plotFromData(folder, graphName):
+    agents = []
+    for filename in os.listdir(folder):
+        tmpdirectory = "{}/{}".format(folder, filename)
+        scores = np.genfromtxt("{}/scores.csv".format(tmpdirectory), delimiter=",")
+        ts = np.genfromtxt("{}/timeSteps.csv".format(tmpdirectory), delimiter=",")
+        ep = np.genfromtxt("{}/epsilon.csv".format(tmpdirectory), delimiter=",")
+        agents.append([filename, scores, ts, ep, filename])
+    
+    
+    plot(agents, graphName)
+
+
+        
+
+folder = "graphData/11"
+filename = "Test_graph.png"
+plotFromData(folder, filename)
